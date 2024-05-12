1e6ce16d3972197b2039c60f44160a7615aa10d0
William <69386111+MrBisquit@users.noreply.github.com>
2024-05-12 17:09:09 +0100

    Delete include/config.hpp

--- include/config.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-static constexpr const char* wifi_ssid = nullptr;
-static constexpr const char* wifi_pass = nullptr;
-static constexpr const char* time_server_domain = "pool.ntp.org";
-static constexpr const unsigned int time_refresh_interval = 10*60;
-
-static const gfx::open_font& text_font = OpenSans_Regular;
-
-// Other stuff (Testing)
